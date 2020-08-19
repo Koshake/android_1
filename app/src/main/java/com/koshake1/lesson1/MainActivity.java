bfc4981fa3db85f2ef979d20cede8b3970148dd2
Koshake <koshake1@mail.com>
2020-08-19 13:38:56 +0300

    toast/log/saving

--- app/src/main/java/com/koshake1/lesson1/MainActivity.java
+++ app/src/main/java/com/koshake1/lesson1/MainActivity.java
@@ -1,12 +1,15 @@
 package com.koshake1.lesson1;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.content.Intent;
 import android.widget.ImageButton;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity{
 
@@ -14,7 +17,6 @@ public class MainActivity extends AppCompatActivity{
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         Button buttonSelectCity = findViewById(R.id.buttonSelectCity);
         buttonSelectCity.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -30,7 +32,64 @@ public class MainActivity extends AppCompatActivity{
                 onButtonSettingsClicked();
             }
         });
+        Log.d("MainActivity", "onCreate()");
+        Toast.makeText(getApplicationContext(), "onCreate()", Toast.LENGTH_SHORT).show();
+    }
 
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Toast.makeText(getApplicationContext(), "onDestroy()", Toast.LENGTH_SHORT).show();
+        Log.d("MainActivity", "onDestroy()");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Toast.makeText(getApplicationContext(), "onStart()", Toast.LENGTH_SHORT).show();
+        Log.d("MainActivity", "onStart()");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Toast.makeText(getApplicationContext(), "onStop()", Toast.LENGTH_SHORT).show();
+        Log.d("MainActivity", "onStop()");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Toast.makeText(getApplicationContext(), "onPause()", Toast.LENGTH_SHORT).show();
+        Log.d("MainActivity", "onPause()");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Toast.makeText(getApplicationContext(), "onResume()", Toast.LENGTH_SHORT).show();
+        Log.d("MainActivity", "onResume()");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Toast.makeText(getApplicationContext(), "onRestart()", Toast.LENGTH_SHORT).show();
+        Log.d("MainActivity", "onRestart()");
+    }
+
+    @Override
+    protected void onSaveInstanceState(@NonNull Bundle outState) {
+        super.onSaveInstanceState(outState);
+        Toast.makeText(getApplicationContext(), "onSaveInstantState()", Toast.LENGTH_SHORT).show();
+        Log.d("MainActivity", "onSaveInstanceState()");
+    }
+
+    @Override
+    protected void onRestoreInstanceState(@NonNull Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        Toast.makeText(getApplicationContext(), "onSaveRestoreState()", Toast.LENGTH_SHORT).show();
+        Log.d("MainActivity", "onRestoreInstanceState()");
     }
 
     private void onButtonSelectCityClicked() {
