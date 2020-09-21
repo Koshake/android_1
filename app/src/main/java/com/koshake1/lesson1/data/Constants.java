49e8134998855d6445554ff316293d86a49bfb31
Koshake <koshake1@mail.com>
2020-09-21 10:08:08 +0300

    threads

--- app/src/main/java/com/koshake1/lesson1/data/Constants.java
+++ app/src/main/java/com/koshake1/lesson1/data/Constants.java
@@ -13,5 +13,13 @@ import java.util.List;
 public interface Constants {
     String CITY_RESULT = "City";
     String URL_YANDEX_WEATHER = "https://yandex.ru/pogoda/saint-petersburg";
-
+    String KEY_CITY = "key_city";
+    String KEY_READ_RESULT = "key_result";
+    String KEY_WEATHER_RESULT = "key_weather_result";
+    String KEY_WEATHER_REQUEST = "key_weather_request";
+    String KEY_RESULT_CITY = "key_city_result";
+    String KEY_RESULT_TEMP = "key_temp_result";
+    String KEY_RESULT_MIN_TEMP= "key_min_temp_result";
+    String KEY_RESULT_MAX_TEMP = "key_max_temp_result";
+    String KEY_RESULT_DESCRIPTION = "key_description_result";
 }
